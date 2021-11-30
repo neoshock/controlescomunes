4e644089dbc655adcce2dd73de6bc27c737614ce
neoshock <pideun@gmail.com>
2021-11-30 11:00:31 -0500

    Practica 1 Creacion de layouts y el activity

--- app/src/main/java/com/example/trabajouno/MainActivity.java
+++ app/src/main/java/com/example/trabajouno/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.trabajouno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String genero = "Masculino";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +19,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private void enviarDatos(View view){
+    public void enviarDatos(View view){
+        Bundle bundle = new Bundle();
+        Intent intent = new Intent(MainActivity.this, Bienvenida.class);
+
         EditText nombre = (EditText) findViewById(R.id.nombreTxt);
-        RadioGroup genero = (RadioGroup)findViewById(R.id.generosRg);
-        genero.check(R.id.masculinoRb);
+        EditText telefono = (EditText) findViewById(R.id.telefonoTxt);
+        EditText fechaNacimiento = (EditText) findViewById(R.id.fechaNacimientoDate);
+
+        bundle.putString("nombre", nombre.getText().toString());
+        bundle.putString("telefono", telefono.getText().toString());
+        bundle.putString("fecha", fechaNacimiento.getText().toString());
+        bundle.putString("genero", genero);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.masculinoRb:
+                if (checked)
+                    genero = "Masculino";
+                    break;
+            case R.id.femeninoRb:
+                if (checked)
+                    genero = "Femenino";
+                    break;
+        }
     }
 }
