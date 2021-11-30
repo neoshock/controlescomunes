14445076b515228aea9bd75b117cf1e27e3da2be
neoshock <pideun@gmail.com>
2021-11-30 11:34:45 -0500

    Practica 1 Agregación de comentarios

--- app/src/main/java/com/example/trabajouno/Bienvenida.java
+++ app/src/main/java/com/example/trabajouno/Bienvenida.java
@@ -11,17 +11,23 @@ public class Bienvenida extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenida);
+
+        //Llamamos a la función para la obtncion de datos del Activity anterior
         getDataFromMainAcivity();
     }
 
     void getDataFromMainAcivity(){
+
+        //Llamada a los text view del activiy Bienvenida
         TextView nombrePersona = (TextView) findViewById(R.id.nombreResultTxt);
         TextView generoPersona = (TextView) findViewById(R.id.generoResultTxt);
         TextView fechaPersona = (TextView) findViewById(R.id.fechaResultTxt);
         TextView telefonoPersona = (TextView) findViewById(R.id.telefonoResultTxt);
 
+        //Obtenemos los datos del activity anterior
         Bundle bundle = this.getIntent().getExtras();
 
+        //Insertamos los datos obtenidos a los view text de este activity
         nombrePersona.setText("Hola!, Bienvenido " + bundle.getString("nombre"));
         generoPersona.setText("Género: " + bundle.getString("genero"));
         fechaPersona.setText("Fecha Nacimiento: " + bundle.getString("fecha"));
--- app/src/main/java/com/example/trabajouno/MainActivity.java
+++ app/src/main/java/com/example/trabajouno/MainActivity.java
@@ -18,28 +18,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Evento que se ejecuta al hacere clic en el boton del Main Activity
     public void enviarDatos(View view){
         Bundle bundle = new Bundle();
+
+        //Llamada al activity Bienvenida
         Intent intent = new Intent(MainActivity.this, Bienvenida.class);
 
+        //Recoger los valores de los imput text
         EditText nombre = (EditText) findViewById(R.id.nombreTxt);
         EditText telefono = (EditText) findViewById(R.id.telefonoTxt);
         EditText fechaNacimiento = (EditText) findViewById(R.id.fechaNacimientoDate);
 
-        RadioButton masculino = (RadioButton) findViewById(R.id.masculinoRb);
         RadioButton femenino = (RadioButton) findViewById(R.id.femeninoRb);
 
+        //Determinar si cual genero esta seleccionado
         if(femenino.isChecked()){
             genero = "Femenino";
         }else {
             genero = "Masculino";
         }
 
+        //Agregamos los campos ingresados en los Inputs al bundle
         bundle.putString("nombre", nombre.getText().toString());
         bundle.putString("telefono", telefono.getText().toString());
         bundle.putString("fecha", fechaNacimiento.getText().toString());
         bundle.putString("genero", genero);
+
+        //Ingresamos la coleccion de datos al intent
         intent.putExtras(bundle);
+
+        //Llamada a a la siguiente actividad
         startActivity(intent);
     }
 }
