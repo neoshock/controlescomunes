ba6dc4bc1e63a03a2a6198ee272c8d27d32c73d2
neoshock <pideun@gmail.com>
2021-11-30 11:24:56 -0500

    Practica 1 Implemntación de envio de datos

--- app/src/main/java/com/example/trabajouno/Bienvenida.java
+++ app/src/main/java/com/example/trabajouno/Bienvenida.java
@@ -3,6 +3,7 @@ package com.example.trabajouno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Bienvenida extends AppCompatActivity {
 
@@ -10,5 +11,20 @@ public class Bienvenida extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenida);
+        getDataFromMainAcivity();
+    }
+
+    void getDataFromMainAcivity(){
+        TextView nombrePersona = (TextView) findViewById(R.id.nombreResultTxt);
+        TextView generoPersona = (TextView) findViewById(R.id.generoResultTxt);
+        TextView fechaPersona = (TextView) findViewById(R.id.fechaResultTxt);
+        TextView telefonoPersona = (TextView) findViewById(R.id.telefonoResultTxt);
+
+        Bundle bundle = this.getIntent().getExtras();
+
+        nombrePersona.setText("Hola!, Bienvenido " + bundle.getString("nombre"));
+        generoPersona.setText("Género: " + bundle.getString("genero"));
+        fechaPersona.setText("Fecha Nacimiento: " + bundle.getString("fecha"));
+        telefonoPersona.setText("Telefono: " + bundle.getString("telefono"));
     }
 }
--- app/src/main/java/com/example/trabajouno/MainActivity.java
+++ app/src/main/java/com/example/trabajouno/MainActivity.java
@@ -7,11 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String genero = "Masculino";
+    private String genero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,28 +26,20 @@ public class MainActivity extends AppCompatActivity {
         EditText telefono = (EditText) findViewById(R.id.telefonoTxt);
         EditText fechaNacimiento = (EditText) findViewById(R.id.fechaNacimientoDate);
 
+        RadioButton masculino = (RadioButton) findViewById(R.id.masculinoRb);
+        RadioButton femenino = (RadioButton) findViewById(R.id.femeninoRb);
+
+        if(femenino.isChecked()){
+            genero = "Femenino";
+        }else {
+            genero = "Masculino";
+        }
+
         bundle.putString("nombre", nombre.getText().toString());
         bundle.putString("telefono", telefono.getText().toString());
         bundle.putString("fecha", fechaNacimiento.getText().toString());
         bundle.putString("genero", genero);
         intent.putExtras(bundle);
         startActivity(intent);
-    }
-
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.masculinoRb:
-                if (checked)
-                    genero = "Masculino";
-                    break;
-            case R.id.femeninoRb:
-                if (checked)
-                    genero = "Femenino";
-                    break;
-        }
     }
 }
